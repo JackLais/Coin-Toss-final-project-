2a3e43bcf509106ed6e7b93c7d017b8de5fa2076
JackLais <jlais@bsd220.org>
2021-11-15 10:39:21 -0600

    Initial Commit

---
+++ Coin Toss (final project)/Coin_Toss__final_project_App.swift
@@ -0,0 +1,17 @@
+//
+//  Coin_Toss__final_project_App.swift
+//  Coin Toss (final project)
+//
+//  Created by Student on 11/15/21.
+//
+
+import SwiftUI
+
+@main
+struct Coin_Toss__final_project_App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
